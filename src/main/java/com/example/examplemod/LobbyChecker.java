608f63ea903f70cc93319a8aaa189119732c06ef
GoodGuyO <o2267278119@outlook.com>
2026-05-11 11:57:59 +0800

    123

---
+++ src/main/java/com/example/examplemod/LobbyChecker.java
@@ -0,0 +1,28 @@
+package com.example.examplemod;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class LobbyChecker {
+    public static  String lobbyName;
+    public LobbyChecker() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    @SubscribeEvent
+    public void checkLobby(TickEvent.ClientTickEvent e){
+        Minecraft mc = Minecraft.getMinecraft();
+        EntityPlayerSP player=mc.thePlayer;
+        if(e.side!= Side.CLIENT||e.type!= TickEvent.Type.CLIENT||e.phase!= TickEvent.Phase.START){
+            return;
+        }
+        if(player==null){
+            lobbyName="Not In Game";
+            return;
+        }
+        mc.ingameGUI.
+    }
+}
