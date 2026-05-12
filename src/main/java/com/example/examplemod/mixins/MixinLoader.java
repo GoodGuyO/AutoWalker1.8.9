3b98ef06768cf5b17b8478074f4ffbfdb9502088
GoodGuyO <o2267278119@outlook.com>
2026-05-12 18:40:29 +0800

    123

--- src/main/java/com/example/examplemod/mixins/MixinLoader.java
+++ src/main/java/com/example/examplemod/mixins/MixinLoader.java
@@ -1,8 +1,5 @@
 package com.example.examplemod.mixins;
 
-import com.example.examplemod.AutoWalk.PathFinder;
-import com.example.examplemod.AutoWalk.Walker;
-import com.example.examplemod.LobbyChecker;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
